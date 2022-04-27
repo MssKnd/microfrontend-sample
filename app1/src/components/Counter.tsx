3c6e47277f6938e21154da9d8873f4710dee1705
Masashi Kondo <mss.knd@gmail.com>
2022-04-28 08:45:03 +0900

    14

---
+++ app1/src/components/Counter.tsx
@@ -0,0 +1,35 @@
+
+/*
+  -----------------------------------
+  nothing particularly salient here
+  this is just an ordinary component.
+  -----------------------------------
+*/
+
+import { Text, Button, Flex } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Flex gap="1rem" direction="column">
+      <Text>
+        {/*
+          I use this little customization to differentiate
+          between app-1 and app-2
+        */}
+        Add by one each click <strong>APP-1</strong>
+      </Text>
+      <Text>Your click count : {count} </Text>
+      
+      {/*
+        I use this little customization to differentiate
+        between app-1 and app-2      
+      */}
+      <Button onClick={() => setCount(count + 1)}>Click me</Button>
+    </Flex>
+  );
+};
+
+export default Counter;
