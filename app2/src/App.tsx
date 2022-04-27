18274e1e6c63eec45d4a90d3980476120a4ff1bc
Masashi Kondo <mss.knd@gmail.com>
2022-04-28 08:47:28 +0900

    16

--- app2/src/App.tsx
+++ app2/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import { Box } from '@chakra-ui/react'
+import Counter from './components/Counter';
+
+const App = () => {
+  return <Box margin="1.2rem">
+    {/*
+      Cosmetic change from app-1/src/App.tsx
+    */}
+    <Box>Serving from App-2</Box>
+    <Box>
+      {/*
+        ------------------------------
+        This is the exported component 
+        ------------------------------
+      */}
+      <Counter />
+    </Box>
+  </Box>
+};
 
 export default App;
